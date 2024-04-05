807f0cb19475c6d2ffa482ff07df190059d419c3
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-04-05 11:03:35 +0200

    Update FR_02_01 - Wyznacz promień.cpp

--- FR_02_01 - Wyznacz promień.cpp
+++ FR_02_01 - Wyznacz promień.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-int main() 
+int main()
 {
 	int xa;
 	int xb;
@@ -11,18 +11,18 @@ int main()
 	int yb;
 	int xc;
 	int yc;
-	
+
 	cin >> xa >> ya >> xb >> yb >> xc >> yc;
-	
-	double ab=sqrt(pow((xb-xa),2)+pow((yb-ya),2));
-	double bc=sqrt(pow((xc-xb),2)+pow((yc-yb),2));
-	double ca=sqrt(pow((xa-xc),2)+pow((ya-yc),2));
-	double p=0.5*(ab+bc+ca);
-	
-	
-	double r=((ab*bc*ca)/sqrt((p*(p-ab)*(p-bc)*(p-ca))));
-	double r4=r/4;
-	
-	cout << fixed  << setprecision(2) <<  r4;
+
+	double ab = sqrt(pow((xb - xa), 2) + pow((yb - ya), 2));
+	double bc = sqrt(pow((xc - xb), 2) + pow((yc - yb), 2));
+	double ca = sqrt(pow((xa - xc), 2) + pow((ya - yc), 2));
+	double p = 0.5 * (ab + bc + ca);
+
+
+	double r = ((ab * bc * ca) / sqrt((p * (p - ab) * (p - bc) * (p - ca))));
+	double r4 = r / 4;
+
+	cout << fixed << setprecision(2) << r4;
 	return 0;
 }
