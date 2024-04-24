13821643052d5a7476a5135d175cc6e6f3605932
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-04-24 22:55:02 +0200

    Create FR_12_11 - Fraktaliusz Cezar.cpp

---
+++ FR_12_11 - Fraktaliusz Cezar.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int x;
+	string napis;
+	cin >> x;
+	while (x--)
+	{
+		cin >> napis;
+		if (napis == "FRAKTAL" || napis == "GSBLUBM" || napis == "HTCMVCN" || napis == "IUDNWDO" || napis == "JVEOXEP" || napis == "KWFPYFQ" || napis == "LXGQZGR" || napis == "MYHRAHS" || napis == "NZISBIT" || napis == "OAJTCJU" || napis == "PBKUDKV" || napis == "QCLVELW" || napis == "RDMWFMX" || napis == "SENXGNY" || napis == "TFOYHOZ" || napis == "UGPZIPA" || napis == "VHQAJQB" || napis == "WIRBKRC" || napis == "XJSCLSD" || napis == "YKTDMTE" || napis == "ZLUENUF" || napis == "AMVFOVG" || napis == "BNWGPWH" || napis == "COXHQXI" || napis == "DPYIRYJ" || napis == "EQZJSZK")
+			cout << "tak" << endl;
+		else
+			cout << "nie" << endl;
+	}
+	return 0;
+}
