ef3c64e229a20f7c6c0a53f558ef1667d6acffb6
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-06-08 22:30:53 +0200

    Create WWO_01_12 - Ukryta wiadomość.cpp

---
+++ WWO_01_12 - Ukryta wiadomość.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Pandemia (Covid-19) jest jak wojna swiatowa, z ta roznica, "
+        << "ze wszyscy walczymy po jednej stronie.";
+    return 0;
+}
