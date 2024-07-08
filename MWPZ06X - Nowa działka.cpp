1a9498f75f1f629edb74dd7f5499c68bb0bf80c6
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-07-08 18:05:10 +0200

    Create MWPZ06X - Nowa działka.cpp

---
+++ MWPZ06X - Nowa działka.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+	int x;
+	cin >> x;
+	int a;
+	int b;
+
+
+	for (int i = 1; i <= x; i++)
+	{
+		cin >> a;
+		cout << pow(a, 2) << endl;
+
+	}
+
+
+
+
+	return 0;
+}
