0dd268916c9f02848b74abf8641bd2c08218a9bd
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-07-08 18:16:50 +0200

    Create POTSAM - Samolot.cpp

---
+++ POTSAM - Samolot.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	cin >> a >> b >> c >> d;
+
+	cout << (a * b) + (c * d) << endl;
+
+
+
+	return 0;
+}
