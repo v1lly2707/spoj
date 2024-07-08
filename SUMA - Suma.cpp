899a4316fec9e1c4c6ae1d7a0376e2ef6bda5a25
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-07-08 18:36:34 +0200

    Create SUMA - Suma.cpp

---
+++ SUMA - Suma.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	long long x;
+	long long suma = 0;
+	while (cin >> x)
+	{
+		suma += x;
+		cout << suma << endl;
+	}
+
+
+	return 0;
+}
