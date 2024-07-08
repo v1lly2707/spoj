fd8ffd43548b3bb33008b42037d4e2ebf2d9792c
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-07-08 18:40:21 +0200

    Create WWO_01_14 - Pies i jego właściciel.cpp

---
+++ WWO_01_14 - Pies i jego właściciel.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+	double z;
+	cin >> x >> y >> z;
+	double t = z / y;
+	cout << fixed << setprecision(2) << x * t;
+
+
+
+
+
+	return 0;
+}
