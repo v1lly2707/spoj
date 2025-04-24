50d0bd9b1bc182856eb04ac26783c57fe4000f7b
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2025-04-24 09:55:29 +0200

    Create TABDWZAD1 - Create table.cpp

---
+++ TABDWZAD1 - Create table.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	
+	for(int i = 0; i < a; i++)
+	{
+		for(int j = 0; j < b; j++)
+			cout << 5 << ' ';
+			
+		cout << '\n';
+	}
+		
+	return 0;
+}
